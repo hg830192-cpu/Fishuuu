067170729653891864ad935491f9d18e5e623c0b
hg830192-cpu <hg830192@gmail.com>
2026-02-08 13:11:06 +0530

    Update Success.tsx

--- components/Success.tsx
+++ components/Success.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import Confetti from 'react-confetti';
 import { useWindowSize } from 'react-use';
-import myLove from '../components/my-love.png';   // ✅ correct import
+import myLove from './my-love.png';   // ✅ correct import
 
 interface SuccessProps {
   onUnlock: () => void;
